7aaa85f9552c0ea6706120cbf8f650459fbee373
Bartosz Ziółkowski <bartosz@remaster.studio>
2026-06-19 10:14:43 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mp = new HashMap<>();
+        mp.put('(', ')');
+        mp.put('{', '}');
+        mp.put('[', ']');
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && mp.get(stack.peek()) == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
