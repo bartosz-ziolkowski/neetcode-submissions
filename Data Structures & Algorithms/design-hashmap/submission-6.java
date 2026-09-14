5a9c9b9f562e35bb1f08f76d0e5b2e87b1134d01
Bartosz Ziółkowski <bartosz@remaster.studio>
2026-09-14 17:25:42 +0200

    Add: design-hashmap - submission-6

---
+++ Data Structures & Algorithms/design-hashmap/submission-6.java
@@ -0,0 +1,29 @@
+class MyHashMap {
+
+    private int[] mp;
+
+    public MyHashMap() {
+        mp = new int[10001];
+        Arrays.fill(mp, -1);     
+    }
+    
+    public void put(int key, int value) {
+        this.mp[key] = value;
+    }
+    
+    public int get(int key) {
+        return this.mp[key];
+    }
+    
+    public void remove(int key) {
+        this.mp[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
