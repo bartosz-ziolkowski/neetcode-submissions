4005506d0acdec01ba912c207e9973bf3b9eda40
Bartosz Ziółkowski <bartosz@remaster.studio>
2026-09-17 17:28:26 +0200

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] pre: prerequisites) {
+            indegree[pre[0]]++;
+            adj.get(pre[1]).add(pre[0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
+        int taken = 0;
+        int[] output = new int[numCourses];
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            output[taken++] = node;
+            for (int course: adj.get(node)) {
+                indegree[course]--;
+                if (indegree[course] == 0) q.offer(course);
+            }
+        }
+
+        return taken == numCourses ? output : new int[0];
+    }
+}
