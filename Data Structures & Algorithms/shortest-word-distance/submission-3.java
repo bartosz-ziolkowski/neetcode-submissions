6dbac62e51951248ea10f61e9cc04cfd3c985672
Bartosz Ziółkowski <bartosz@remaster.studio>
2026-09-22 19:53:53 +0200

    Add: shortest-word-distance - submission-3

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int idx1 = -1;
+        int idx2 = -1;
+        int dist = wordsDict.length;
+
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (word1.equals(wordsDict[i])) {
+                idx1 = i;
+            } else if (word2.equals(wordsDict[i])) {
+                idx2 = i;
+            }
+
+            if (idx1 != -1 && idx2 != -1) {
+                dist = Math.min(dist, Math.abs(idx1 - idx2));
+            }
+        }
+
+        return dist;
+
+    }
+}
